6fe3561b603f78dded910501bdf47841cff9a062
SerhiiDubovets <Sergio.d@bigmir.net>
2022-07-08 12:06:17 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import Feedback from "./feedback/feedback";
+import { GlobalStyle } from "./GlobalStyle";
+import Section from "./Section";
+import FeedbackOptions from "./FeedbackOptions";
+import Statistics from "./Statistics";
+import Notification from "./Notification";
 
 
 class App extends Component {
@@ -8,14 +12,45 @@ class App extends Component {
     neutral: 0,
     bad: 0  
   }
-  render(){
+
+  handleIncrement = (evt) => {
+    const stateName = evt.currentTarget.name;
+    this.setState(prev => ({ [stateName]: prev[stateName] + 1 }));
+    console.log(evt.currentTarget);
+  }
+
+  countTotalFeedback () {
+    return Object.values(this.state).reduce((total, item) => total + item, 0) 
+       
+       }
+
+       countPositiveFeedbackPercentage  () {
+        return  Math.round((this.state.good / this.countTotalFeedback ()) * 100)
+     }
+
+     
+     render(){
+    const btn = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+
     return (
       <div>
-        <Feedback 
-          good={this.state.good} 
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-        />
+      <GlobalStyle/>
+      <Section title={"Please leave feedback"}>
+      <FeedbackOptions 
+          options={btn} 
+          onLeaveFeedback={this.handleIncrement}
+      />
+      </Section> 
+        
+      
+      {this.countTotalFeedback ()? <Statistics 
+          good={good} 
+          neutral={neutral} 
+          bad={bad} 
+          total={this.countTotalFeedback()} 
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+       /> : <Notification message={"There is no feedback"}/>}
       </div>
     );
   }
